18f6853485668b14b49809e2662c83560326ce9e
jorge1407 <jorgelon99@gmail.com>
2020-03-17 12:05:34 +0100

    Barra de navegacion ~ Launcher

---
+++ cliente/src/components/Launcher.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+
+class Launcher extends Component{
+    render(){
+        return(
+            <div className="contenedor">
+                <div className="jumbotron mt-5">
+                    <div className="col-sm-8 mx-auto">BIENVENIDO</div>
+                </div>
+            </div>
+        )
+    }
+}
